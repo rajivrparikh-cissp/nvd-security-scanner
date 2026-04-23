8e074ebb75a48b85cf55fdbca83e61859ae85b5d
rajivrparikh-cissp <rajivrparikh@gmail.com>
2026-04-23 19:35:13 +1000

    Add Google AdSense script

--- nvd-scanner-web/src/app/layout.tsx
+++ nvd-scanner-web/src/app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6560924171188758"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
